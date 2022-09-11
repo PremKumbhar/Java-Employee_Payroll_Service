c6c97b38e9cd6570316bb2ef92e45fa306609ca5
PremKumbhar <kumbharprem7738@gmail.com>
2022-09-11 20:11:23 +0530

    [Prem]:Added demonstrate file operation

--- src/main/java/com/bridgelabz/EmployeePayrollService/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService/EmployeePayrollService.java
@@ -1,18 +1,20 @@
 package com.bridgelabz.EmployeePayrollService;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
     private ArrayList<EmployeePayroll> employeePayrollList;
 
-    public EmployeePayrollService(){
-        employeePayrollList = new ArrayList<>();
+    public EmployeePayrollService()
+    {
+        employeePayrollList = new ArrayList<EmployeePayroll>();
     }
 
     public static void main(String[] args) {
         System.out.println("Employee payroll service program");
-
+        ArrayList<EmployeePayroll> employeePayrollsList = new ArrayList<>();
         EmployeePayrollService employeePayrollService = new EmployeePayrollService();
         employeePayrollService.readEmployeePayrollData();
         employeePayrollService.writeEmployeePayrollData();
@@ -32,4 +34,14 @@ public class EmployeePayrollService {
         System.out.println("writing employee payroll data on console :");
         System.out.println(employeePayrollList);
     }
+    public static boolean deleteFiles(File contentsToDelete) {
+        File[] allContents = contentsToDelete.listFiles();
+        if (allContents != null){
+            for (File file : allContents){
+                deleteFiles(file);
+            }
+        }
+        return contentsToDelete.delete();
+    }
+
 }
